96db376ed21cee1e21ba1e45ea93e1352de2d603
Raphaël Lelièvre <raphael1.lelievre@gmail.com>
2025-11-28 09:41:13 +0100

    chore: Add empty project

---
+++ src/main/java/com/example/projet_raphael_lelievre/ProjetRaphaelLelievreApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet_raphael_lelievre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetRaphaelLelievreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetRaphaelLelievreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projet_raphael_lelievre/ProjetRaphaelLelievreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_raphael_lelievre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetRaphaelLelievreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
